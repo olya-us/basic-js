ddc169c6dd234658ecf0d8ba6e69345168036dfa
Olya <o.us@sw-tech.by>
2024-12-09 14:25:00 +0300

    feat: complete vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,84 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(val = true) {
+    this.val = val;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  charWithNum = {};
+
+  fillObj() {
+    for (let i = 0; i < this.alphabet.length; i++) {
+      let key = this.alphabet[i];
+      this.charWithNum[key] = i;
+    }
+    return this.charWithNum;
+  }
+
+  encrypt(text, keyWord) {
+    if (typeof text === 'undefined' || typeof keyWord === 'undefined') {
+      throw new Error("Incorrect arguments!");
+    }
+
+    this.fillObj();
+    text = text.toUpperCase();
+    keyWord = keyWord.toUpperCase();
+    let code = "";
+    let numText = 0;
+    let numKey = 0;
+    let count = 0;
+
+    for (let i = 0; i < text.length; i++) {
+      numText = this.charWithNum[text[i]];
+
+      if (typeof numText !== "number") {
+        count++;
+        code += text[i];
+      } else {
+        numKey = this.charWithNum[keyWord[(i - count) % keyWord.length]];
+        code += this.alphabet[(numText + numKey) % this.alphabet.length];
+      }
+    }
+
+    if (this.val === false) {
+      code = code.split("").reverse().join("");
+    }
+
+    return code;
+  }
+
+  decrypt(text, keyWord) {
+    if (typeof text === 'undefined' || typeof keyWord === 'undefined') {
+      throw new Error("Incorrect arguments!");
+    }
+
+    this.fillObj();
+    text = text.toUpperCase();
+    keyWord = keyWord.toUpperCase();
+    let code = "";
+    let numText = 0;
+    let numKey = 0;
+    let count = 0;
+
+    for (let i = 0; i < text.length; i++) {
+      numText = this.charWithNum[text[i]];
+
+      if (typeof numText !== "number") {
+        count++;
+        code += text[i];
+      } else {
+        numKey = this.charWithNum[keyWord[(i - count) % keyWord.length]];
+        code += this.alphabet[(numText - numKey + this.alphabet.length) % this.alphabet.length];
+      }
+    }
+
+    if (this.val === false) {
+      code = code.split("").reverse().join("");
+    }
+
+    return code;
+
   }
 }
 
