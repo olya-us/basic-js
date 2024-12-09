a4f91022bf50714761b0c58c3265428a011afeb0
Olya <o.us@sw-tech.by>
2024-12-09 12:52:58 +0300

    feat: complete Dns stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const result = {};
+
+  for (const domain of domains) {
+    const parts = domain.split('.').reverse();
+    let dns = '';
+
+    for (const part of parts) {
+      dns += `.${part}`;
+      result[dns] = (result[dns] || 0) + 1;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
